a0c61c544f42d2b4ed784e8f1966a0c6c01cba95
luuquocbao56132 <luuquocbao56132@gmail.com>
2024-07-30 16:26:22 +0700

    Splash Screen
    First init splash screen and project

---
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.bookstore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.bookstore.ui.theme.BookstoreTheme
+
+@ExperimentalAnimationApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        installSplashScreen().setKeepOnScreenCondition {
+//            !splashViewModel.isLoading.value
+//        }
+//
+//        setContent {
+//            OnBoardingComposeTheme {
+//                val screen by splashViewModel.startDestination
+//                val navController = rememberNavController()
+//                SetupNavGraph(navController = navController, startDestination = screen)
+//            }
+//        }
+        installSplashScreen().setKeepOnScreenCondition { true }
+    }
+}
