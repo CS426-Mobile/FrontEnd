98c4f19df5ac9c0d3af038b6222adf3afc03a596
Trịnh Nguyễn Thảo Vi <tntvi22@apcs.fitus.edu.vn>
2024-09-02 15:09:38 +0700

    Split Login (CustomTextField to components)

--- app/src/main/java/com/example/bookstore/Login.kt
+++ app/src/main/java/com/example/bookstore/Login.kt
@@ -9,57 +9,66 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material3.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.focusTarget
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.bookstore.components.CustomTextField
+import com.example.bookstore.data.AppDatabase
+import com.example.bookstore.model.User
+import com.example.bookstore.repository.UserRepository
 import com.example.bookstore.ui.theme.blurOrange
 import com.example.bookstore.ui.theme.errorColor
 import com.example.bookstore.ui.theme.mainColor
+import com.example.bookstore.viewmodel.UserViewModel
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MaterialTheme {
                 Surface {
-                    LoginNavHost()
+                    // Initialize UserViewModel using viewModel() function
+//                    val userViewModel: UserViewModel = viewModel()
+                    val userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+
+                    LoginNavHost(userViewModel)
                 }
             }
         }
@@ -67,40 +76,47 @@ class LoginActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginNavHost() {
+fun LoginNavHost(userViewModel: UserViewModel) {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "sign_in") {
-        composable("sign_in") { SignInScreen(navController) }
-        composable("sign_up") { SignUpScreen(navController) }
+        composable("sign_in") { SignInScreen(navController, userViewModel) }
+        composable("sign_up") { SignUpScreen(navController, userViewModel) }
     }
 }
 
 @Composable
-fun SignInScreen(navController: NavController) {
+fun SignInScreen(navController: NavController, userViewModel: UserViewModel) {
+//    val userViewModel: UserViewModel = viewModel()
+
     SignInUI(
         onSignInSuccess = {
-//            navController.context.startActivity(Intent(navController.context, HomeActivity::class.java))
+//             Navigate to HomeActivity or any other screen on successful sign-in
+            navController.context.startActivity(Intent(navController.context, OnboardingActivity::class.java))
         },
         onSignUpClick = {
             navController.navigate("sign_up")
-        }
+        },
+        userViewModel = userViewModel
     )
 }
 
 @Composable
-fun SignUpScreen(navController: NavController) {
+fun SignUpScreen(navController: NavController, userViewModel: UserViewModel) {
+//    val userViewModel: UserViewModel = viewModel()
+
     SignUpUI(
         onSignUpSuccess = {
             navController.navigate("sign_in") {
                 popUpTo("sign_up") { inclusive = true }
             }
-        }
+        },
+        userViewModel = userViewModel
     )
 }
 
 @Composable
-fun SignInUI(onSignInSuccess: () -> Unit, onSignUpClick: () -> Unit) {
+fun SignInUI(onSignInSuccess: () -> Unit, onSignUpClick: () -> Unit, userViewModel: UserViewModel) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var errorMessage by remember { mutableStateOf("") }
@@ -108,7 +124,6 @@ fun SignInUI(onSignInSuccess: () -> Unit, onSignUpClick: () -> Unit) {
     val isFormValid = email.isNotBlank() && password.isNotBlank()
     val focusManager = LocalFocusManager.current
 
-    // Wrapper Box to detect taps outside of the text fields
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -212,10 +227,12 @@ fun SignInUI(onSignInSuccess: () -> Unit, onSignUpClick: () -> Unit) {
             Button(
                 onClick = {
                     if (isFormValid) {
-                        if (email == "example@gmail.com" && password == "password") {
-                            onSignInSuccess()
-                        } else {
-                            errorMessage = "Incorrect password or account does not exist"
+                        userViewModel.getUser(email, password) { user ->
+                            if (user != null) {
+                                onSignInSuccess()
+                            } else {
+                                errorMessage = "Incorrect password or account does not exist"
+                            }
                         }
                     }
                 },
@@ -257,7 +274,7 @@ fun SignInUI(onSignInSuccess: () -> Unit, onSignUpClick: () -> Unit) {
 }
 
 @Composable
-fun SignUpUI(onSignUpSuccess: () -> Unit) {
+fun SignUpUI(onSignUpSuccess: () -> Unit, userViewModel: UserViewModel) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
@@ -278,7 +295,7 @@ fun SignUpUI(onSignUpSuccess: () -> Unit) {
     ) {
         // Feather Image
         Image(
-            painter = painterResource(id = R.drawable.feather), // replace with actual feather image
+            painter = painterResource(id = R.drawable.feather),
             contentDescription = "Feather",
             modifier = Modifier
                 .padding(bottom = 0.dp)
@@ -306,7 +323,6 @@ fun SignUpUI(onSignUpSuccess: () -> Unit) {
             label = "Email or Phone number",
             icon = R.drawable.ic_email
         )
-
         Spacer(modifier = Modifier.height(8.dp))
 
         // Password TextField
@@ -314,7 +330,6 @@ fun SignUpUI(onSignUpSuccess: () -> Unit) {
             value = password,
             onValueChange = { password = it },
             label = "Password",
-            isError = errorMessage.isNotEmpty(),
             isPassword = true,
             icon = R.drawable.ic_password
         )
@@ -325,149 +340,40 @@ fun SignUpUI(onSignUpSuccess: () -> Unit) {
             value = confirmPassword,
             onValueChange = { confirmPassword = it },
             label = "Confirm Password",
-            isError = errorMessage.isNotEmpty(),
             isPassword = true,
             icon = R.drawable.ic_password
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
         // Sign Up Button
         Button(
             onClick = {
                 if (isFormValid) {
                     if (password == confirmPassword) {
+                        userViewModel.insertUser(User(0, email, password))
                         onSignUpSuccess()
                     } else {
                         errorMessage = "Passwords do not match"
                     }
+                } else {
+                    errorMessage = "All fields are required"
                 }
             },
             colors = ButtonDefaults.buttonColors(
-                backgroundColor = if (isFormValid) mainColor else blurOrange, // Set the color based on form validity
+                backgroundColor = if (isFormValid) mainColor else blurOrange,
                 contentColor = Color.White
             ),
-            shape = RoundedCornerShape(24.dp), // Rounded corners for the button
+            shape = RoundedCornerShape(24.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp)
         ) {
             Text(
                 text = "Sign up",
-                fontSize = 18.sp, // Increase font size for the button text
-                fontWeight = FontWeight.Bold, // Make the text bold
-                color = Color.White // Ensure the text color is white
-            )
-        }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        // Sign In Text
-        Row {
-            Text(
-                text = "Already have an account?",
-                color = Color.Gray
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = "Sign in",
+                fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.clickable { onSignUpSuccess() },
-                color = mainColor
+                color = Color.White
             )
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CustomTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    label: String,
-    isError: Boolean = false,
-    isPassword: Boolean = false,
-    icon: Int? = null,
-) {
-    val focusRequester = remember { FocusRequester() }
-    val isFocused = remember { mutableStateOf(false) }
-    var passwordVisible by remember { mutableStateOf(false) }
-
-    OutlinedTextField(
-        value = value,
-        onValueChange = onValueChange,
-        label = {
-            Text(
-                text = label,
-                color = when {
-                    isError -> errorColor
-                    isFocused.value -> mainColor
-                    else -> Color.Gray
-                }
-            )
-        },
-        isError = isError,
-        visualTransformation = if (isPassword && !passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
-        keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,
-        trailingIcon = {
-            if (isPassword) {
-                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                    Icon(
-                        painter = painterResource(
-                            id = if (passwordVisible) R.drawable.ic_visibility else R.drawable.ic_visibility_off
-                        ),
-                        contentDescription = if (passwordVisible) "Hide password" else "Show password",
-                        tint = when {
-                            isError -> errorColor
-                            else -> Color.Gray
-                        },
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-            }
-        },
-        leadingIcon = {
-            icon?.let {
-                Icon(
-                    painter = painterResource(id = it),
-                    contentDescription = null,
-                    tint = when {
-                        isError -> errorColor
-                        isFocused.value -> mainColor
-                        else -> Color.Gray
-                    },
-                    modifier = Modifier.size(24.dp)
-                )
-            }
-        },
-        textStyle = TextStyle(
-            fontSize = 18.sp,
-            color = if (isError) errorColor else Color.Black
-        ),
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = if (isError) errorColor else mainColor,
-            unfocusedBorderColor = if (isError) errorColor else Color.Gray,
-            backgroundColor = if (isFocused.value) Color.White else Color.Transparent
-        ),
-        shape = RoundedCornerShape(32.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .onFocusChanged { focusState -> isFocused.value = focusState.isFocused }
-            .focusRequester(focusRequester)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview1() {
-    MaterialTheme {
-        SignInUI(onSignInSuccess = {}, onSignUpClick = {})
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    MaterialTheme {
-        SignUpUI(onSignUpSuccess = {})
     }
 }
