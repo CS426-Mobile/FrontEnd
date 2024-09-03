97ffd80b0f1fec00030d29fddaf5fdfd0befe62b
Trịnh Nguyễn Thảo Vi <tntvi22@apcs.fitus.edu.vn>
2024-09-03 10:54:52 +0700

    Basic UI for Home DB test

--- app/src/main/java/com/example/bookstore/HomeActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bookstore
-
-class HomeActivity {
-
-}
---
+++ app/src/main/java/com/example/bookstore/screen/Account.kt
@@ -0,0 +1,12 @@
+package com.example.bookstore.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bookstore.viewmodel.HomeViewModel
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun AccountScreen(viewModel: HomeViewModel = viewModel()) {
+
+}
